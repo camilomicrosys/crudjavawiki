b1a0be0afc40071c8e0a6aa84335db7399f7ea5d
camilo <camiloagudelo090@gmail.com>
2023-02-26 22:12:54 -0500

    version1

---
+++ src/main/java/com/mycompany/crudwikipedia/Crudwikipedia.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crudwikipedia;
+
+import ventanas.RegistroAlumnos;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Crudwikipedia {
+
+    public static void main(String[] args) {
+
+  
+   
+       //esto para poder mostrar el formulario
+        RegistroAlumnos ventana= new RegistroAlumnos();
+        ventana.show();
+    
+    }
+}
